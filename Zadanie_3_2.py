b2700768f4526718961a46eed5a59f3c24cdefe9
monas1975 <t_szymanski@onet.eu>
2025-06-25 10:17:00 +0200

    Init project

---
+++ Zadanie_3_2.py
@@ -0,0 +1,17 @@
+
+#deklaracje
+shopping_dict = {
+    "warzywniak": ["buraki", "ziemniaki", "truskawki", "czereśnie"],
+    "piekarnia": ["bułka", "chleb", "bagietka"]
+}
+towar=""
+product_counter =0
+
+
+for key, values in shopping_dict.items():
+    for product in values:
+        towar = towar + ", " + product.capitalize()
+        product_counter +=1
+    print("Idę do sklepu " + key.capitalize() + " po:" + towar)
+    towar =""
+    print("w sumie kupuję " + str(product_counter) + " produkty")
